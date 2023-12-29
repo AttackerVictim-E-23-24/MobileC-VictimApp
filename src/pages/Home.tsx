8998e4d757eb14e7540783713e13b7667830f6a2
Angelo Torella <angelotorella2002@gmail.com>
2023-12-29 04:28:28 -0400

    vista de network

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,43 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {  IonHeader,  IonTitle, IonToolbar, 
+  IonPage,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardContent,
+  IonCardTitle,} from '@ionic/react';
+import './css/Home.css';
+  
+import NetworkView from '../View/NetworkView'; // Importa NetworkView
 
-const Tab1: React.FC = () => {
+const Home: React.FC = () => {
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Attacker App</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
+      <IonContent fullscreen >
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Attacker App</IonTitle>
           </IonToolbar>
         </IonHeader>
+
+        
+        <IonCard>
+          <IonCardHeader>
+              <IonCardTitle>Último envío de ubicación</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+            <NetworkView/>
+          </IonCardContent>
+        </IonCard>
+
       </IonContent>
     </IonPage>
   );
 };
 
-export default Tab1;
+export default Home;
